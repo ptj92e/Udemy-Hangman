a3271394ac6c78074eba1e43a9dac37063fcdbf8
Phillip Jones <ptj92e@gmail.com>
2020-06-12 17:01:16 -0500

    Setup Methods

---
+++ Hangman/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+
+namespace Hangman
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            StartGame();
+            PlayGame();
+            EndGame();
+        }
+
+        private static void StartGame()
+        {
+            Console.WriteLine("Starting Game");
+            AskForUsername();
+        }
+
+        private static void AskForUsername()
+        {
+            Console.WriteLine("Asking User For Name");
+        }
+
+        private static void PlayGame()
+        {
+            Console.WriteLine("Playing Game");
+            DisplayMaskedWord();
+            AskForLetter();
+        }
+
+        private static void DisplayMaskedWord()
+        {
+            Console.WriteLine("Display the masked word");
+        }
+
+        private static void AskForLetter()
+        {
+            Console.WriteLine("Ask for letter.");
+        }
+
+        private static void EndGame()
+        {
+            Console.WriteLine("Game Over");
+        }
+    }
+}
