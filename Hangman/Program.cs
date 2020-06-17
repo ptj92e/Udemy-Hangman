8346ac1f62b1005116422807db155fa0b8b7ddf2
Phillip Jones <ptj92e@gmail.com>
2020-06-17 13:35:44 -0500

    Guessing letters works

--- Hangman/Program.cs
+++ Hangman/Program.cs
@@ -6,6 +6,8 @@ namespace Hangman
     {
         static string name;
         static int numberOfGuesses;
+        static string correctWord = "hangman";
+        static char[] letters;
 
         public static void Main(string[] args)
         {
@@ -16,39 +18,72 @@ namespace Hangman
 
         private static void StartGame()
         {
-            Console.WriteLine("Starting game...");
+            letters = new char[correctWord.Length];
+            for (int i = 0; i < correctWord.Length; i++)
+                letters[i] = '-';
+
             AskForUsername();
-            if (name.Length <= 2)
-                Console.WriteLine("Your name must be longer than 2 characters");
-                AskForUsername();
         }
 
         static string AskForUsername()
         {
             Console.WriteLine("What is your name?");
-            return name = Console.ReadLine();
+            string input = Console.ReadLine();
+
+            if (input.Length >= 2)
+                return name = input;
+            else
+                Console.WriteLine("Your name should be more than 2 characters.");
+                return AskForUsername();
         }
 
         private static void PlayGame()
         {
-            Console.WriteLine("Playing Game");
-            DisplayMaskedWord();
-            AskForLetter();
+            do
+            {
+                Console.Clear();
+                DisplayMaskedWord();
+                char guessedLetter = AskForLetter();
+                CheckLetter(guessedLetter);
+            } while (correctWord != new string(letters));
+        }
+
+        private static void CheckLetter(char guessedLetter)
+        {
+            for (int i = 0; i < correctWord.Length; i++)
+            {
+                if(guessedLetter == correctWord[i])
+                {
+                    letters[i] = guessedLetter;
+                }
+            }
         }
 
         private static void DisplayMaskedWord()
         {
-            Console.WriteLine("Display the masked word");
+            foreach (char c in letters)
+            {
+                Console.Write(c);
+            }
         }
 
-        private static void AskForLetter()
+        private static char AskForLetter()
         {
-            Console.WriteLine("Ask for letter.");
+            string input;
+            do
+            {
+                Console.WriteLine("Enter a letter:");
+                input = Console.ReadLine();
+            } while (input.Length != 1);
+
             numberOfGuesses++;
+
+            return input[0];
         }
 
         private static void EndGame()
         {
+            Console.Clear();
             Console.WriteLine("Game Over");
             Console.WriteLine($"Thank you for playing {name}");
             Console.WriteLine($"You made {numberOfGuesses} guesses.");
